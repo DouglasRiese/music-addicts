1952f05db25a8fc28dda655408a8b09840f4192f
Douglas Riese <riese2119@saskpolytech.ca>
2026-04-03 12:48:38 -0600

    Moved API to separate repository and moved FE to root directory

--- functions/melody-mastery-api/src/decorator/RouteDefinition.ts
+++
@@ -1,5 +0,0 @@
-export interface RouteDefinition {
-    param: string; // the uuid for the entity
-    method: string; // HTTP method like 'post or 'delete'
-    action: string; // the method in the controller object (like 'save' or 'delete')
-}
